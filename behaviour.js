968113ce2fd5b4cc179d73c52311b781a0e842ab
Cameron Miller <cameron.miller294@gmail.com>
2018-10-12 04:19:32 -0400

    Updates
    Update elements, content, and navigation

--- behaviour.js
+++ behaviour.js
@@ -30,24 +30,29 @@ function displayContentsMain(titleCase) {
 
   switch (titleCase) {
     case 'About':
-      dT = "(About Marissa here)";
+      dT = "Marissa is a Certified Holistic Wellness Practitioner, Holistic Nutrition Specialist, Transformational Wellness Coach, Guided Mediation Facilitator, and Certified Yoga Instructor. She specializes in weight loss, stress management and supplements. Her work is based off the ancient medical system rooted in India- Ayurveda. Ayurveda is a science backed, effective Holistic medical system that has been used for over 5 thousand years. Marissa's experience with Ayurveda has been life changing. Using the wisdom of Ayurveda, Marissa has overcame an anxiety and panic disorder, low immunity and other health conditions caused by excess inflammation in the body. She has increased her mental capacity by utilizing herbs and Ayurvedic practices for the brain. She is ready to share these ancient wellness solutions with the world!";
       detailsText = document.createTextNode(dT);
       break;
 
     case 'My Work':
-      dT = "(My Work here)";
+      dT = "Please note that every service I offer is completely customized.";
       detailsText = document.createTextNode(dT);
       title.classList.add('software-title');
       break;
 
     case 'Schedule':
-      dT = "(My current schedule)";
+      dT = "To schedule a one on one assessment, please leave your email in the space provided. I will contact you within 24 hours to schedule a session.";
       detailsText = document.createTextNode(dT);
       title.classList.add('software-title');
       break;
 
     case 'Contact':
-      dT = "(How to contact me)";
+      dT = "Reach out share a wellness goal.";
+      detailsText = document.createTextNode(dT);
+      break;
+
+    case 'Resources':
+      dT = "Saveable list Here";
       detailsText = document.createTextNode(dT);
       break;
   }
@@ -57,6 +62,11 @@ function displayContentsMain(titleCase) {
   titleHere.appendChild(title);
   details.appendChild(detailsText);
   detailsHere.appendChild(details);
+  let infoLeft = document.getElementsByClassName('info-left')[0];
+  infoLeft.style.width = '35%';
+  let circleContainer = document.getElementsByClassName('circle-container')[0];
+  circleContainer.style.transform = 'scale(0.8)';
+  //circleContianer.style.top = '-10px';
 
   return([titleHere, detailsHere]);
 }
@@ -64,6 +74,10 @@ function displayContentsMain(titleCase) {
 function displayAbout() {
   let mainArray = displayContentsMain('About');
   mainArray;
+
+  let check = 'Check out her <a href="https://www.youtube.com/channel/UCG47NLyWHWd9j31EmQupCVQ?view_as=subscriber" target="_blank">Youtube channel</a> to learn more about Ayurveda';
+
+  mainArray[1].innerHTML += (check);
 
   mainArray[0].style.animation = 'beseen 1s forwards';
   mainArray[1].style.animation = 'beseen 1s forwards';
@@ -73,6 +87,13 @@ function displayMyWork() {
   let mainArray = displayContentsMain('My Work');
   mainArray;
 
+  let content = workContent();
+  let i = 0;
+  while (i < content.length) {
+    mainArray[1].appendChild(content[i]);
+    i++;
+  }
+
   mainArray[0].style.animation = 'beseen 1s forwards';
   mainArray[1].style.animation = 'beseen 1s forwards';
 }
@@ -80,6 +101,9 @@ function displayMyWork() {
 function displaySchedule() {
   let mainArray = displayContentsMain('Schedule');
   mainArray;
+
+  //let content = scheduleContent();
+  //mainArray[1].appendChild(content);
 
   mainArray[0].style.animation = 'beseen 1s forwards';
   mainArray[1].style.animation = 'beseen 1s forwards';
@@ -89,8 +113,129 @@ function displayContact() {
   let mainArray = displayContentsMain('Contact');
   mainArray;
 
+  let content = contactContent();
+  mainArray[1].appendChild(content);
+
   mainArray[0].style.animation = 'beseen 1s forwards';
   mainArray[1].style.animation = 'beseen 1s forwards';
+}
+
+function displayResources() {
+  let mainArray = displayContentsMain('Resources');
+  mainArray;
+
+  mainArray[0].style.animation = 'beseen 1s forwards';
+  mainArray[1].style.animation = 'beseen 1s forwards';
+}
+
+function workContent() {
+  function workTab(titleT, paraT) {
+    this.title = document.createElement("h4");
+    this.tText = document.createTextNode(titleT);
+    this.para = document.createElement("p");
+    this.pText = document.createTextNode(paraT);
+    this.div = document.createElement("div");
+  }
+
+  var myWorkTabs = [
+    ['Assessment', "Discover your body type! Let's figure out which areas of wellness and lifestyle need the most attention. This assessment will give you a clear outline of your current state of wellbeing. The Ancients believed that every bodily system is interconnected, meaning if one is out of balance, there's likely an imbalance in another system. Together, with the knowledge of the Ancients, we can determine the current state of wellness and begin with small action steps that promote complete balance. Don't worry though, the path to balance is gentle."],
+
+    ['Customized Weight Protocol', "Weight loss is not only running on the treadmill or counting calories. Utilize my weight loss protocol and discover the delicious foods that could assist in weight loss and an improved metabolism. This protocol comes with a meal plan, recipe support, weekly coaching, and a full day's routine to promote weight loss. *please ask about fitness support*"],
+    ['Customized Meal Plan', 'This meal plan goes hand in hand with the outcome of your assessment. Upon determining your body type and current state, I will take another assessment on your food preferences and build you a customized plan based on your goals (weight loss, increased energy, low sugar, etc). This comes with the meal plan, grocery shopping and recipe support.'],
+    ['Customized Supplement Plan', 'Each customized supplement plan comes with an assessment and the top 10 supplements I believe could assist you with your goals and support with choosing which brands are best.'],
+    ['Customized Guided Meditation', 'After a short assessment, I will formulate a guided meditation that will assist with your desired goals. Mediation can be used to achieve specific goals, re-program our "automatic" thoughts, reduce stress, clear the mind, or just simply relax.']
+  ];
+
+  var allWorkTabs = [];
+
+  for (var i = 0; i < myWorkTabs.length; i ++) {
+    let tabName = new workTab(myWorkTabs[i][0], myWorkTabs[i][1]);
+
+    tabName.title.classList.add("my-work-c-title");
+    tabName.title.appendChild(tabName.tText);
+    tabName.title.setAttribute("onclick", "expandWorkContent( " + i + ")");
+
+    tabName.para.classList.add("my-work-c-para");
+    tabName.para.appendChild(tabName.pText);
+
+    tabName.div.classList.add("my-work-c-div");
+    tabName.div.appendChild(tabName.title);
+    tabName.div.appendChild(tabName.para);
+
+
+    allWorkTabs.push(tabName.div);
+  }
+
+  return(allWorkTabs);
+}
+
+function expandWorkContent(t) {
+  let container = document.getElementsByClassName('my-work-c-div')[t];
+  let style = getComputedStyle(container);
+  let cHeight = style.getPropertyValue('height');
+  let para = document.getElementsByClassName('my-work-c-para')[t];
+  let newH = getStyleValue(para, 'height');
+  if (cHeight == '260px') {
+    container.style.height = 'calc(30px + 10px)';
+    setTimeout(function(){ para.style.display = 'none'; }, 250);
+    setTimeout(function(){ para.style.opacity = '0'; }, 0);
+  } else {
+    setTimeout(function(){ para.style.display = 'block'; }, 250);
+    container.style.height = '260px';
+    setTimeout(function(){ para.style.opacity = '1'; }, 300);
+  }
+}
+
+function getStyleValue(ele, style) {
+  let el = getComputedStyle(ele).getPropertyValue(style);
+  return el;
+}
+
+function scheduleContent() {
+  let cont = document.createElement("div");
+  let para = document.createElement("p");
+  let pText = document.createTextNode("To schedule a one on one assessment, please leave your email in the space provided. I will contact you within 24 hours to schedule a session.");
+  para.appendChild(pText);
+  cont.appendChild(para);
+  return(cont);
+}
+
+function contactContent() {
+  let nameInp = document.createElement("input");
+  let emailInp = document.createElement("input");
+  let cityInp = document.createElement("input");
+  let textBox = document.createElement("textarea");
+  let contactForm = document.createElement("div");
+  let formUL = document.createElement("ul");
+
+  let inputs = [nameInp, emailInp, cityInp, textBox];
+
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].classList.add('contact-input');
+    let placeholder;
+    switch(i) {
+      case 0:
+        placeholder = 'Name';
+        break;
+      case 1:
+        placeholder = 'Email';
+        break;
+      case 2:
+        placeholder = 'City';
+        break;
+      case 3:
+        placeholder = 'Wellness Goal';
+        break;
+    }
+    inputs[i].setAttribute("placeholder", placeholder);
+    let formLI = document.createElement("li");
+    formLI.classList.add('contact-li');
+
+    formLI.appendChild(inputs[i]);
+    formUL.appendChild(formLI);
+    contactForm.appendChild(formUL);
+  }
+  return(contactForm);
 }
 
 function changeElement(el, aClicked) {
@@ -105,28 +250,39 @@ function changeElement(el, aClicked) {
   var newCenterImg, newOuterImg;
   switch (el) {
     case 'fire':
-      bg = 'var(--fire-bg)';
+      //bg = 'var(--fire-bg)';
+      bg = 'radial-gradient(rgb(255, 240, 93), rgb(255, 93, 70))';
       newCenterImg = 'fires.png';
       centerImg.alt = outerImgAlt;
       loadFire();
       break;
     case 'water':
-      bg = 'var(--water-bg)';
+      //bg = 'var(--water-bg)';
+      bg = 'radial-gradient(rgb(0, 184, 233), rgb(8, 0, 191))';
       newCenterImg = 'waters.png';
       centerImg.alt = outerImgAlt;
       loadWater();
       break;
     case 'earth':
-      bg = 'var(--earth-bg)';
+      //bg = 'var(--earth-bg)';
+      bg = 'radial-gradient(rgb(82, 233, 0), rgb(0, 120, 5))';
       newCenterImg = 'earths.png';
       centerImg.alt = outerImgAlt;
       loadEarth();
       break;
     case 'air':
-      bg = 'var(--air-bg)';
+      //bg = 'var(--air-bg)';
+      bg = 'radial-gradient(rgb(255, 120, 220), rgb(152, 0, 156))';
       newCenterImg = 'airs.png';
       centerImg.alt = outerImgAlt;
       loadAir();
+      break;
+    case 'space':
+      //bg = 'var(--space-bg)';
+      bg = 'radial-gradient(rgb(107, 90, 121), rgb(36, 25, 45))';
+      newCenterImg = 'spaces.png';
+      centerImg.alt = outerImgAlt;
+      loadSpace();
       break;
   }
 
@@ -185,6 +341,11 @@ function displayElementContents(element) {
       dT = "Earth info";
       detailsText = document.createTextNode(dT);
       break;
+
+    case 'Space':
+      dT = "Space info";
+      detailsText = document.createTextNode(dT);
+      break;
   }
 
   title.classList.add('current-title');
@@ -219,6 +380,13 @@ function loadAir() {
 
 function loadEarth() {
   let mainArray = displayElementContents('Earth');
+  mainArray;
+  mainArray[0].style.animation = 'beseen 1s forwards';
+  mainArray[1].style.animation = 'beseen 1s forwards';
+}
+
+function loadSpace() {
+  let mainArray = displayElementContents('Space');
   mainArray;
   mainArray[0].style.animation = 'beseen 1s forwards';
   mainArray[1].style.animation = 'beseen 1s forwards';
